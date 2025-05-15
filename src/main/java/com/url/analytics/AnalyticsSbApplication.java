d04f3b913407e82538cfe43aad5e1a763576961e
Montek <87750128+Montekkundan@users.noreply.github.com>
2025-05-15 14:14:40 -0700

    Initial commit with models

---
+++ src/main/java/com/url/analytics/AnalyticsSbApplication.java
@@ -0,0 +1,13 @@
+package com.url.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/url/analytics/AnalyticsSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
