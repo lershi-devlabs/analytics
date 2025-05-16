82389e23f08ab1de3c502e2c1a0530cf8c461388
Montek <87750128+Montekkundan@users.noreply.github.com>
2025-05-15 18:57:26 -0700

    get all urls for a user

--- src/main/java/com/url/analytics/controller/UrlMappingController.java
+++ src/main/java/com/url/analytics/controller/UrlMappingController.java
@@ -7,12 +7,10 @@ import com.url.analytics.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -33,4 +31,13 @@ public class UrlMappingController {
         UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl, user);
         return ResponseEntity.ok(urlMappingDTO);
     }
+
+    @GetMapping("/myurls")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<UrlMappingDTO>> getUsersUrls(Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        List<UrlMappingDTO> url = urlMappingService.getUrlsByUser(user);
+        return ResponseEntity.ok(url);
+    }
 }
+
