e85eea5b01600067771aa1f671ddc69900ea77e3
Montek <87750128+Montekkundan@users.noreply.github.com>
2025-05-15 18:03:10 -0700

    shortening the url

---
+++ src/main/java/com/url/analytics/repository/UrlMappingRepository.java
@@ -0,0 +1,15 @@
+package com.url.analytics.repository;
+
+import com.url.analytics.models.UrlMapping;
+import com.url.analytics.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    UrlMapping findByShortUrl(String shortUrl);
+    List<UrlMapping> findByUser(User user);
+}
